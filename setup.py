39c68fa1463c33571c3bd958d88bc1e6af80dbee
Neha Vishwakarma <nehavishwakarma7777@gmail.com>
2023-09-24 03:03:33 +0530

    Mongo update

--- setup.py
+++ setup.py
@@ -8,17 +8,8 @@ Author ="Neha Vishwakarma"
 Email ="nehavishwakarma7777@gmail.com"
 
 # This function return list of requirements
-def get_all_requirements() -> List[str]:
-    requirements_list: List[str] = []
-
-    # Open the requirements.txt file for reading
-    with open('requirements.txt', 'r') as file:
-        for line in file:
-            # Remove leading and trailing whitespace and append to the list
-            requirement = line.strip()
-            if requirement:
-                requirements_list.append(requirement)
-
+def get_all_requirements()->List[str]:
+    requirements_list:List[str] =[]
     return requirements_list
 
 setup(
@@ -29,7 +20,3 @@ setup(
     packages= find_packages(),
     install_requires =get_all_requirements(),
 )
-
-if __name__ == "__main__":
-    requirements = get_all_requirements()
-    print(requirements)
