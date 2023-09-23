42f5f1833885aa3269a824830d7b6dcc422b410c
Neha Vishwakarma <106228976+NeHa77A@users.noreply.github.com>
2023-09-24 03:05:31 +0530

    Update mongoDB_connection.py

--- sensor/configuration/mongoDB_connection.py
+++ sensor/configuration/mongoDB_connection.py
@@ -9,7 +9,7 @@ class MongoDBClient:
     def __init__(self, database_name=DATABASE_NAME) ->None:
         try:
             if MongoDBClient.client is None:
-                mongo_db_url ="mongodb+srv://Neha:Neha@cluster0.jsogkox.mongodb.net/"
+                mongo_db_url ="mongodb+srv://Neha:<password>@cluster0.jsogkox.mongodb.net/"
                 """
                 mango_db_url =os.getenv(MONGODB_URL_KEY)
                 if mongo_db_url is None:
