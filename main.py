80b11b8678e59119c5551d8e41a4c0ead0fc5143
Neha Vishwakarma <nehavishwakarma7777@gmail.com>
2023-09-26 04:29:26 +0530

    data ingestion

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from sensor.configuration.mongoDB_connection import MongoDBClient
 from sensor.exception import SensorException
 import sys,os
 from sensor.logger import logging
+from sensor.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig
 
+"""
 def test_exce():
     try:
         logging.info("We are diving by zero")
@@ -16,3 +18,8 @@ if __name__ =='__main__':
         print(e)
     #mongodb_client=MongoDBClient()
     #print("collection name: ",mongodb_client.database.list_collection_names())
+"""
+if __name__=='__main__':
+    training_pipeline_config =TrainingPipelineConfig()
+    data_ingestion_config = DataIngestionConfig(training_pipeline_config=training_pipeline_config)
+    print(data_ingestion_config.__dict__)
